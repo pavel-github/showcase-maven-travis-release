7e69db9dd3c9fc2b66081c8611a059adf46eb982
pavel <36902936+pavel-github@users.noreply.github.com>
2018-10-29 16:18:20 +0100

    Fix greeting message for pr-trigger branch

--- src/main/java/demo/CliMain.java
+++ src/main/java/demo/CliMain.java
@@ -2,7 +2,7 @@ package demo;
 
 public class CliMain {
   public static void main(String... args) {
-    System.out.println("Hello from MASTER branch!");
+    System.out.println("Hello from PR-TRIGGER branch!");
     // empty line
   }
 }
