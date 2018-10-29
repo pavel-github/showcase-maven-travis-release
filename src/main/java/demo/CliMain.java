abd229310f05c641f333ba8f6d375c4edd9f28b8
pavel <36902936+pavel-github@users.noreply.github.com>
2018-10-29 14:55:59 +0100

    Fix greeting in master branch

--- src/main/java/demo/CliMain.java
+++ src/main/java/demo/CliMain.java
@@ -2,6 +2,6 @@ package demo;
 
 public class CliMain {
   public static void main(String... args) {
-    System.out.println("Hello from DEVELOP-2 branch!");
+    System.out.println("Hello from MASTER branch!");
   }
 }
